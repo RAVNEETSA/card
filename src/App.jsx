7149e4f2e7869f74cfe9c809e279fc963515d09b
RAVNEETSA <ravneetkaurk130@gmail.com>
2024-03-06 15:55:10 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import About from './assets/About'
+
+export default function App() {
+  return (
+    <div>
+      <About/>
+    </div>
+  )
+}
+
---
+++ src/assets/About.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <>
+    <div className='row'>
+    <div class="card col mx-1">
+    
+<img 
+src="https://cdn.iconscout.com/icon/free/png-256/free-c-59-1175248.png"/>
+<h5 class="card-title">C Language</h5>
+<div class="card-body">
+    
+    <p >
+    C is a general-purpose computer programming language. It was created in the 1970s by Dennis Ritchie, and remains very widely used and influential.</p>
+    <a href="#" class="btn btn-primary"> learn more</a>
+  </div>
+    </div>
+    <div class="card col mx-1">
+<img 
+src="https://cdn.icon-icons.com/icons2/3398/PNG/512/plus_logo_c_icon_214621.png"/>
+<h5 class="card-title">C++Language</h5>
+<div class="card-body">
+<p >
+C++ (or “C-plus-plus”) is a generic programming language for building software. It's an object-oriented language. In other words, it emphasizes using data fields with unique attributes (a.k.a. objects) rather than logic or functions.</p>
+    <a href="#" class="btn btn-primary"> learn more</a>
+  </div>
+</div> 
+<div class="card col mx-1">
+<img 
+src="https://upload.wikimedia.org/wikipedia/commons/d/d2/C_Sharp_Logo_2023.svg"/>
+<h5 class="card-title">C sharp</h5>
+<div class="card-body">
+<p >
+C# (C-Sharp) is a programming language developed by Microsoft that runs on the .NET Framework. C# is used to develop web apps, desktop apps, mobile apps, games ...</p>
+    <a href="#" class="btn btn-primary"> learn more</a>
+  </div>
+    </div>
+    <div class="card col mx-1">
+<img 
+src="https://www.shutterstock.com/image-vector/python-logo-programming-language-icon-600w-1764554297.jpg"/>
+<h5 class="card-title">python</h5>
+<div class="card-body">
+ <p >
+ Python is a popular programming language. It was created by Guido van Rossum, and released in 1991. It is used for: web development (server-side), ...</p>
+ <a href="#" class="btn btn-primary"> learn more</a>
+  </div>
+    </div>
+    <div class="card col mx-1">
+<img 
+src="https://i.pinimg.com/736x/79/5e/bb/795ebb5f4a470cd7242136237f61fc53.jpg"/>
+<h5 class="card-title">java</h5>
+<div class="card-body">
+    
+    <p >
+    Java is a widely-used programming language for coding web applications. It has been a popular choice among developers for over two decades, with millions of Java applications in use today. Java is a multi-platform,</p>
+    <a href="#" class="btn btn-primary"> learn more</a>
+  </div>
+
+    </div>
+    </div>
+    </>
+  )
+}
